112003062b3b3e270bca234848a6603accec077d
haydencal <148795760+haydencal@users.noreply.github.com>
2023-10-26 01:16:56 -0400

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class sample {
+	@Test
+	void locationTest() {
+		String location = "Tinkham Veale";
+		assertEquals("Tinkham Veale", location);
+		fail("Location does not match");
+	}
+	
+}
+
