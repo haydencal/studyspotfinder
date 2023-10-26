997572176d3ffb2054d0f5e3cfa1f29bbc7078b9
haydencal <148795760+haydencal@users.noreply.github.com>
2023-10-25 22:27:09 -0400

    methods n stuff

--- Map.java
+++ Map.java
@@ -2,5 +2,12 @@ public class Map
 {
     public Map(){
     }
+    public Map makeMap(){
+        Map map = new Map();
+        return map;
+    }
+    public String[] makePath(Building b1, Building b2){
+        String[] buildingPath = new String[99];
+        return buildingPath;
+    }
 }
-
