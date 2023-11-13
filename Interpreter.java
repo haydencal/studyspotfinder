befcd94fd42f6b81fd9fd214b463a909339d6a4a
Mariana Davis <lmd117@case.edu>
2023-11-13 01:57:15 -0500

    started on building methods
    started to create building methods and fixed one of the coords in the map

--- Interpreter.java
+++ Interpreter.java
@@ -1,7 +1,14 @@
 public class Interpreter
 {
+    
     String userInput;
     public Interpreter(String in){
         userInput = in;
     }
+
+    public void selectBuilding(String name) {
+
+    }
+
+    
 }
